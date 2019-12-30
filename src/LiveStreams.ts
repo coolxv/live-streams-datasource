dbc0b25e273e1bea183eadc53a3ae30af804ca4e
coolxv <coolxv@gmail.com>
2019-12-30 16:16:45 +0800

    fix

--- src/LiveStreams.ts
+++ src/LiveStreams.ts
@@ -1,33 +1,54 @@
 //import { FieldType, DataQueryResponse, CircularDataFrame } from '@grafana/data';
-import { DataFrame, FieldType, CircularDataFrame } from '@grafana/data';
+import { KeyValue, DataFrame } from '@grafana/data';
 import { Observable } from 'rxjs';
-import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
+import { webSocket } from 'rxjs/webSocket';
 import { LiveStreamsTarget } from './Types';
-import { map } from 'rxjs/operators';
-// import { appendResponseToBufferedData } from './result_transformer';
+import { map, finalize } from 'rxjs/operators';
+import { createResponseBufferedData, appendResponseToBufferedData } from './ResultTransformer';
 
 /**
  * Cache of websocket streams that can be returned as observable. In case there already is a stream for particular
  * target it is returned and on subscription returns the latest dataFrame.
  */
 export class LiveStreams {
+  private streams: KeyValue<Observable<DataFrame[]>> = {};
+
   getStream(target: LiveStreamsTarget): Observable<DataFrame[]> {
-    const data = new CircularDataFrame({ capacity: target.size });
-    data.addField({ name: 'value', type: FieldType.string });
-    let stream: WebSocketSubject<any> = webSocket(target.url);
-    const observableA = stream.multiplex(
-      () => ({ subscribe: 'A' }), // When server gets this message, it will start sending messages for 'A'...
-      () => ({ unsubscribe: 'A' }), // ...and when gets this one, it will stop.
-      message => true // If the function returns `true` message is passed down the stream. Skipped if the function returns false.
-    );
-    let obj = observableA.pipe(
+    let stream = this.streams[target.url];
+    if (stream) {
+      return stream;
+    }
+    const data = createResponseBufferedData(target);
+    stream = webSocket(target.url).pipe(
+      finalize(() => {
+        console.log('delete');
+        delete this.streams[target.url];
+      }),
       map(rsp => {
-        console.log(rsp);
-        let value = Math.random() * 100;
-        data.values.value.add(`${value}`);
+        appendResponseToBufferedData(rsp, data);
         return [data];
       })
     );
-    return obj;
+    this.streams[target.url] = stream;
+    return stream;
   }
+  // getStream(target: LiveStreamsTarget): Observable<DataFrame[]> {
+  //   const data = new CircularDataFrame({ capacity: target.size });
+  //   data.addField({ name: 'value', type: FieldType.string });
+  //   let stream: WebSocketSubject<any> = webSocket(target.url);
+  //   const observableA = stream.multiplex(
+  //     () => ({ subscribe: 'A' }), // When server gets this message, it will start sending messages for 'A'...
+  //     () => ({ unsubscribe: 'A' }), // ...and when gets this one, it will stop.
+  //     message => true // If the function returns `true` message is passed down the stream. Skipped if the function returns false.
+  //   );
+  //   let obj = observableA.pipe(
+  //     map(rsp => {
+  //       console.log(rsp);
+  //       let value = Math.random() * 100;
+  //       data.values.value.add(`${value}`);
+  //       return [data];
+  //     })
+  //   );
+  //   return obj;
+  // }
 }
--- src/ResultTransformer.ts
+++ src/ResultTransformer.ts
@@ -1,10 +1,21 @@
 import _ from 'lodash';
 
-import { MutableDataFrame } from '@grafana/data';
+import { DataFrame, CircularDataFrame, FieldType } from '@grafana/data';
+import { LiveStreamsTarget } from './Types';
+// import { LiveStreamsResponse } from './Types';
 
-import { LiveStreamsResponse } from './Types';
-
-export function appendResponseToBufferedData(response: LiveStreamsResponse, data: MutableDataFrame) {
+export function createResponseBufferedData(target: LiveStreamsTarget): DataFrame {
   // Should we do anything with: response.dropped_entries?
-  data.values.id.add(`test`);
+  console.log(target);
+  const data = new CircularDataFrame({ capacity: target.size });
+  (<any>data).target = target;
+  data.addField({ name: 'value', type: FieldType.string });
+  return data;
+}
+export function appendResponseToBufferedData(rsp: any, data: DataFrame) {
+  // Should we do anything with: response.dropped_entries?
+  console.log(rsp);
+  let value = Math.random() * 100;
+  (data as CircularDataFrame).values.value.add(`${value}`);
+  console.log(data);
 }
